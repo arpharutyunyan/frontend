93f0bf0895402281f4f8b91e9154fe2e7fd5a233
arpharutyunyan <arpine.harutyunyana@gmail.com>
2023-09-11 00:35:47 +0400

    task 17.1 with adaptive

--- homeworks/task17/task17_1/js/script.js
+++ homeworks/task17/task17_1/js/script.js
@@ -3,16 +3,11 @@ AOS.init();
 $('#header_carousel').owlCarousel({
     loop: true,
     center: true,
-    items: 5,
+    items: 1,
     nav: true,
     navText: ['<i class="fa-solid fa-arrow-left-long"></i>', '<i class="fa-solid fa-arrow-right-long"></i>'],
     dots: false,
-    responsiveClass: true,
-    responsive: {
-        1000: {
-            items: 1
-        }
-    }
+    responsiveClass: false,
 });
 
 $('#project__slide').owlCarousel({
@@ -22,8 +17,29 @@ $('#project__slide').owlCarousel({
     nav: true,
     responsiveClass: true,
     responsive: {
-        1000: {
+        320: {
+            items: 1
+        },
+        577: {
+            items: 1
+        },
+        768: {
+            items: 1.5
+        },
+        992: {
+            items: 2
+        },
+        1200: {
+          items: 2.5
+        },
+        1440: {
             items: 3
         }
     }
+});
+
+$(document).ready(function() {
+    $(document).on('click', '.menu-icon', () => {
+        $('.nav').toggle();
+    });
 });
