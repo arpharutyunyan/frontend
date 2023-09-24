a1f37756a3f631a2264615f57df9d7c521abe034
arpharutyunyan <arpine.harutyunyana@gmail.com>
2023-09-24 18:51:37 +0400

    variable

---
+++ js/homework/task2/app.js
@@ -0,0 +1,51 @@
+// ----------------------- 1 -----------------------
+var userInput = 20;
+var result;
+console.log('result before init: ', result);
+
+result = userInput + 5;
+console.log(`${userInput} + 5 = ${result}`);
+
+result = userInput - 10.2;
+console.log(`${userInput} - 10.2 = ${result}`);
+
+result = userInput / 3;
+console.log(`${userInput} / 3 = ${result}`);
+
+result = userInput * 100;
+console.log(`${userInput} * 100 = ${result}`);
+
+result = userInput % 3;
+console.log(`${userInput} % 3 = ${result}`);
+
+result = userInput ** 3;
+console.log(`${userInput} ** 3 = ${result}`);
+
+
+// ----------------------- 2 -----------------------
+var country = 'United States';
+var continent = 'North America';
+var population = 331000000;
+var isIsland = false;
+
+console.log('Country: ' + country);
+console.log('Continent: ' + continent);
+console.log('Population: ' + population);
+console.log('isIsland: ' + isIsland);
+
+
+// ----------------------- 3 -----------------------
+var height = 1.69;
+var mass = 78;
+var bmi = mass / height ** 2;
+console.log('Height: ' + height + ' meters');
+console.log('Mass: ' + mass + ' kilograms');
+console.log('BMI: ' + bmi);
+
+height = 1.95;
+mass = 92;
+bmi = mass / height ** 2;
+console.log('Height: ' + height + ' meters');
+console.log('Mass: ' + mass + ' kilograms');
+console.log('BMI: ' + bmi);
+
