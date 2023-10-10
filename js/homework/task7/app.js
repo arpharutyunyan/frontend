def3679e5cc5accd26de314cbb35436273dde29f
arpharutyunyan <arpine.harutyunyana@gmail.com>
2023-10-10 21:29:03 +0400

    task7

--- js/homework/task7/app.js
+++ js/homework/task7/app.js
@@ -1,82 +1,45 @@
-// ------------------ 1 ------------------
-function compare(num) {
-    return function (comparable_number) {
-        if (num > comparable_number) {
-            return 'big';
-        } else if (num < comparable_number) {
-            return 'small';
-        } else {
-            return 'equal';
-        }
-    };
-}
+let carOne = {};
+carOne.brand = 'Toyota';
+carOne.year = 2015;
+carOne.price = 10000;
 
-let comp = compare(20);
-console.log(comp(10));
-console.log(comp(20));
-console.log(comp(30));
+let carTwo = {
+    'brand': 'Toyota',
+    'year': 2022,
+    'price': 25000,
+};
 
-// ------------------ 2 ------------------
-function factorial(num) {
-    if (num === 0 || num === 1) {
-        return num;
-    }
-
-    return num * factorial(num - 1);
-}
-
-let result = factorial(5);
-console.log(result);
-
-// ------------------ 3 ------------------
-// function fibonacci(number) {
-//
-//     if (number === 1) {
-//         return 0;
-//     } else if (number === 2) {
-//         return 1;
-//     }
-//
-//     return fibonacci(number - 1) + fibonacci(number - 2);
-// }
-//
-// let fibonacci_index = 8;
-// let fibonacci_result = '';
-// if (fibonacci_index > 0) {
-//     for (let i = 1; i <= fibonacci_index; i++) {
-//         if (i === fibonacci_index) {
-//             fibonacci_result += fibonacci(i) + '. ';
-//         } else {
-//             fibonacci_result += fibonacci(i) + ', ';
-//         }
-//     }
-// } else {
-//     fibonacci_result = 'no fibonacci number\'s list index';
-// }
-//
-//
-// console.log(fibonacci_result);
-
-let fibonacci_result = '0, 1, ';
-let a = 0;
-let b = 1;
-
-function fibonacci(index) {
-    if (index <= 0) {
-        fibonacci_result = 'no fibonacci number\'s list index';
-    } else if (index === 2) {
-        return;
-    } else {
-        let sum = a + b;
-        if (index === 3) { // last index should be 3, first 2 element(0, 1) already shown
-            fibonacci_result += `${sum}.`;
-        } else {
-            fibonacci_result += `${sum}, `;
-        }
-        a = b;
-        b = sum;
-        fibonacci(index - 1);
+function print(obj){
+    for(let key in obj){
+        console.log(`${key}: ${obj[key]}`);
     }
 }
 
-console.log(fibonacci_result);
+print(carOne);
+print(carTwo);
+
+carOne.show = function (){
+    console.log(`${this.brand}: ${this.price}`);
+};
+// console.log(carOne);
+carOne.show();
+
+function showCar(){
+    console.log(`${this.brand}: ${this.price}`);
+}
+
+carTwo.show = showCar;
+// console.log(carTwo);
+carTwo.show();
+
+function showCarDetails(){
+    return `Car Object:
+    year: ${this.year},
+    brand: ${this.brand},
+    price: ${this.price}`;
+}
+
+let carOneCall = showCarDetails.call(carOne);
+console.log(carOneCall);
+let carTwoCall = showCarDetails.call(carTwo);
+console.log(carTwoCall);
