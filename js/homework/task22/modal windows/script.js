f98c68174999c7517ca1aadb0f8037cccdc6fd12
arpharutyunyan <arpine.harutyunyana@gmail.com>
2023-11-22 22:16:15 +0400

    task22

--- js/homework/task22/modal windows/script.js
+++ js/homework/task22/modal windows/script.js
@@ -2,26 +2,30 @@
 const showModal = document.querySelector('.show-modal');
 const modal = document.querySelector('.modal');
 const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
 
 if (showModal) {
     showModal.addEventListener('click', () => {
-        modal.classList.toggle('hidden');
-    })
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
 }
 
 if (closeModalBtn) {
     closeModalBtn.addEventListener('click', () => {
         closeModal();
-    })
+    });
 }
 
-document.addEventListener('click', (event)=>{
-    if(event.target != modal && !modal.contains(event.target) && event.target!=showModal){
+if (overlay) {
+    overlay.addEventListener('click', () => {
         closeModal();
-    }
-})
+    });
+}
+
 function closeModal(){
     if(!modal.classList.contains('hidden')){
         modal.classList.add('hidden');
+        overlay.classList.add('hidden');
     }
 }
--- js/homework/task22/slider with data attribute/scripts/main.js
+++ js/homework/task22/slider with data attribute/scripts/main.js
@@ -4,9 +4,37 @@ const prevBtn = document.getElementById("prev_btn");
 const images = document.querySelectorAll(".picture_collection img");
 
 const pictureArray = ["picture_1.png",
-                    "picture_2.jpg",
-                    "picture_3.jpg",
-                    "picture_4.jpg",
-                    "picture_5.jpg"];
+    "picture_2.jpg",
+    "picture_3.jpg",
+    "picture_4.jpg",
+    "picture_5.jpg"];
 
 let imageIndex = 0;
+const pictureCount = pictureArray.length;
+
+nextBtn.addEventListener('click', () => {
+    if (imageIndex !== pictureCount - 1) {
+        imageIndex += 1;
+    } else {
+        imageIndex = 0;
+    }
+
+    generalPicture.src = `images/${pictureArray[imageIndex]}`;
+});
+
+prevBtn.addEventListener('click', () => {
+    if (imageIndex) {
+        imageIndex -= 1;
+    } else {
+        imageIndex = pictureCount - 1;
+    }
+
+    generalPicture.src = `images/${pictureArray[imageIndex]}`;
+});
+
+images.forEach(elem => {
+    elem.addEventListener('click', ()=>{
+        imageIndex = parseInt(elem.dataset.sliderIndex);
+        generalPicture.src = `images/${pictureArray[imageIndex]}`;
+    });
+});
